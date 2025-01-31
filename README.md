b0cf88c93e2689e4f4652b84b0051d2882ced327
piyuzzsh <pkale9532@gmail.com>
2025-01-31 09:47:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Portfolio
+# PiyushPortfolio
---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new typed(".text" , {
+    String:["Web Developer" , "Front-End Developer" , "Photographer"],
+    loop:true,
+    typeSpeed: 100,
+    backspeed:100,
+    backdelay:1000,
+
+})
